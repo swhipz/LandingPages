5dca63d2906d74cf7671f6777d4a3e70bb775b6d
swhipz <fredrik.jensen14@live.no>
2024-12-11 18:14:39 +0100

    simple structure ready

--- 1-ainbox/ainbox-mine/src/App.js
+++ 1-ainbox/ainbox-mine/src/App.js
@@ -4,21 +4,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className="container">
+      <span className="bgColorOne">
+        <header className="headerbar">
+      <h1>Header</h1> {/*to be replaced with a component*/}
+      <nav>here comes navigation i guess</nav> {/*to be replaced with a component*/}
       </header>
+      <section className="heroElement">
+      <h2 className="hook">Designed <br/>to help growing</h2>
+      <p className="ptext">Our comprehensive marketing platform prioretizes your audience's needs.</p>
+      <button>Book Demo</button> {/*to be replaced with a component*/}
+      </section>
+    </span>       
+    <span className="bgColorTwo">
+
+    </span>
+    <span className="bgColorThree">
+    
+    </span>
     </div>
+    </div>
+    
   );
 }
 
