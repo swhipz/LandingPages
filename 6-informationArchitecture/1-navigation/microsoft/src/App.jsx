a5a8c8497d08567d609556e60537a37d64cf9fa5
swhipz <fredrik.jensen14@live.no>
2025-03-01 16:23:27 +0100

    finished mockup

--- 6-informationArchitecture/1-navigation/microsoft/src/App.jsx
+++ 6-informationArchitecture/1-navigation/microsoft/src/App.jsx
@@ -3,9 +3,11 @@ import './App.css';
 import Header from './header/Header';
 import Aside from "./aside/Aside";
 import Main from "./main/Main"
+import backarrow from "./backarrow.svg";
 function App() {
   return (
     <div className="App">
+       <a class="back" href="https://fredrikjensen.no/fredrikjensen/articles/articles/articles.html"><img src={backarrow} alt="back" /></a>
       <Header />
       <div className="collect">
         <Aside />
